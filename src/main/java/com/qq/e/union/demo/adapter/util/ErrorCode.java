86250d1ff3cd1dda46bdc81193b5c88f8b08801e
chenw168000@gmail.com <yingdajiali>
2019-12-04 10:11:00 +0800

    Initial commit

---
+++ src/main/java/com/qq/e/union/demo/adapter/util/ErrorCode.java
@@ -0,0 +1,27 @@
+package com.qq.e.union.demo.adapter.util;
+
+public class ErrorCode {
+  /**
+   * 请求超时错误
+   */
+  public static final int TIME_OUT = 4011;
+  /**
+   * 无广告 Native JS同时维护
+   */
+  public static final int NO_AD_FILL = 5004;
+
+  /**
+   * 开屏广告容器尺寸错误
+   */
+  public static final int SPLASH_CONTAINER_SIZE = 4005;
+
+  /**
+   * 视频素材下载错误
+   */
+  public static final int VIDEO_DOWNLOAD_FAIL = 5002;
+
+  /**
+   * 视频素材播放错误
+   */
+  public static final int VIDEO_PLAY_ERROR = 5003;
+}
